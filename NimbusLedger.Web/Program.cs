55294df48732fa654a7b1789af66b263df00193d
Roberto Gramellini <robertogramellini@hotmail.com>
2025-10-09 22:55:38 +0200

    Revert "Integrate Entra ID Authentication"
    This reverts commit 7b5965f344880c7c44354840672ef6c0f4e3faa6.

--- NimbusLedger.Web/Program.cs
+++ NimbusLedger.Web/Program.cs
@@ -2,11 +2,6 @@ using NimbusLedger.Core.Abstractions;
 using NimbusLedger.Core.Options;
 using NimbusLedger.Infrastructure.Storage;
 using Serilog;
-using Microsoft.Identity.Web;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,26 +15,7 @@ builder.Host.UseSerilog((context, services, configuration) =>
 
 builder.Services.Configure<HybridLedgerOptions>(builder.Configuration.GetSection("HybridLedger"));
 builder.Services.AddSingleton<ILedgerSnapshotStore, FileLedgerSnapshotStore>();
-
-// Authentication & Authorization (Microsoft Entra ID)
-builder.Services
-    .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
-
-builder.Services.AddAuthorization(options =>
-{
-    // Require authenticated users by default
-    options.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-});
-
-builder.Services.AddRazorPages(options =>
-{
-    // Allow anonymous access to specific pages
-    options.Conventions.AllowAnonymousToPage("/Error");
-    options.Conventions.AllowAnonymousToPage("/Privacy");
-});
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -54,36 +30,14 @@ app.UseSerilogRequestLogging();
 
 app.UseRouting();
 
-// AuthN/Z middleware
-app.UseAuthentication();
-app.UseAuthorization();
-
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
-
-// Sign-in/out helpers
-app.MapGet("/signin", async (HttpContext ctx) =>
-{
-    await ctx.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties
-    {
-        RedirectUri = "/"
-    });
-}).AllowAnonymous();
-
-app.MapGet("/signout", async (HttpContext ctx) =>
-{
-    await ctx.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties
-    {
-        RedirectUri = "/"
-    });
-    await ctx.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-}).RequireAuthorization();
 
 app.MapGet("/api/snapshot", async (ILedgerSnapshotStore store, CancellationToken cancellationToken) =>
 {
     var snapshot = await store.GetLatestSnapshotAsync(cancellationToken).ConfigureAwait(false);
     return snapshot is null ? Results.NoContent() : Results.Ok(snapshot);
-}).WithName("GetSnapshot").RequireAuthorization();
+}).WithName("GetSnapshot");
 
 app.Run();
